6ee1899704819fa24657064a2c1a064c3ac2a073
HamedPro3D <110119103+HamedPro3D@users.noreply.github.com>
2025-02-10 07:17:48 +0000

    Arreglo python

--- Python/solve.py
+++ Python/solve.py
@@ -1,25 +1,27 @@
 import time
+
 def burbuja(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n - i - 1):
             if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]  # CORREGIDO
+
 def imprimir_arreglo(arr):
     print(" ".join(map(str, arr)))
+
 arr = [64, 25, 12, 22, 11]
 print("Arreglo original:")
 imprimir_arreglo(arr)
 inicio = time.time()
 burbuja(arr)
 
-# Medir el tiempo después de la ejecución
+# Medir tiempo de finalización
 fin = time.time()
+
 # Mostrar el arreglo ordenado
 print("\nArreglo ordenado:")
 imprimir_arreglo(arr)
 
 # Mostrar el tiempo de ejecución
 print(f"\nTiempo de ejecución: {fin - inicio:.6f} segundos")
-
-#
